8269ab3ccb8008ecb897d0d961e99ccb98eda280
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-20 15:34:57 +0900

    add select component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+
 export default async function Page() {
-  return <>Top Page</>
+  return (
+    <>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </>
+  )
 }
