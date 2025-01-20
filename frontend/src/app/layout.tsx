1679305c2b698dfd6937fe1dba5a06feed7f9acd
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-20 15:43:58 +0900

    add next-themes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import '../global.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
+      <body className="p-4">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
